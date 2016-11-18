55456594d7c34bb064fa851e883e808a21e6b286
lpn90 <leonardo.pinheiro.ti@gmail.com>
2016-11-18 20:02:07 -0200

    Fase2 finalizada

--- app/Entities/Project.php
+++ app/Entities/Project.php
@@ -27,7 +27,7 @@ class Project extends Model implements Transformable
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'owner_id');
     }
 
     public function notes()
